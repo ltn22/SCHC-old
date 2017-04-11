00c7e50bc3cbb3b6b4f5acec732ecec52cbb829e
pclavier92 <pclavier@hotmail.com>
2017-04-11 17:10:06 +0200

    include coap options

--- javascript/test/testing.js
+++ javascript/test/testing.js
@@ -1,5 +1,5 @@
 /**
- * Created by Philippe Clavier on 20/03/2017.
+ * Created by Philippe Clavier on 04/04/2017.
  */
 
 // Rules Definitions
@@ -36,12 +36,9 @@ var rule0 = {
         "compDecompFct": "not-sent",
     },
     "IP_prefixES": {
-        "targetValue": {
-            "1": "20010db80a0b12f0",
-            "2": "2d513de80a0b4df0"
-        },
-        "matchingOperator": "match-mapping",
-        "compDecompFct": "mapping-sent(4)"
+        "targetValue": "20010db80a0b12f0",
+        "matchingOperator": "ignore",
+        "compDecompFct": "not-sent"
     },
     "IP_iidES": {
         "targetValue": "",
@@ -49,12 +46,9 @@ var rule0 = {
         "compDecompFct": "ESiid-DID",
     },
     "IP_prefixLA": {
-        "targetValue": {
-            "1": "20010db80a0b12f0",
-            "2": "2d513de80a0b4df0"
-            },
-        "matchingOperator": "match-mapping",
-        "compDecompFct": "mapping-sent(4)"
+        "targetValue": "2d513de80a0b4df0",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
     },
     "IP_iidLA": {
         "targetValue": "",
@@ -106,10 +100,128 @@ var rule0 = {
         "matchingOperator": "MSB(12)",
         "compDecompFct": "LSB(4)",
     },
-     "CoAP_token": {
+    "CoAP_token": {
         "targetValue": "",
         "matchingOperator": "ignore",
-        "compDecompFct": "value-sent",
+        "compDecompFct": "value-sent"
+    }
+};
+var rule1 = {
+    "IP_version": {
+        "targetValue": "6",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "IP_trafficClass": {
+        "targetValue": "00",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "IP_flowLabel": {
+        "targetValue": "00000",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "IP_payloadLength": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "compute-IPv6-length"
+    },
+    "IP_nextHeader": {
+        "targetValue": "11",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "IP_hopLimit": {
+        "targetValue": "40",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "IP_prefixES": {
+        "targetValue": {
+            "1": "20010db80a0b12f0",
+            "2": "2d513de80a0b4df0"
+        },
+        "matchingOperator": "match-mapping",
+        "compDecompFct": "mapping-sent(4)"
+    },
+    "IP_iidES": {
+        "targetValue": "",
+        "matchingOperator": "equal",
+        "compDecompFct": "ESiid-DID"
+    },
+    "IP_prefixLA": {
+        "targetValue": {
+            "1": "20010db80a0b12f0",
+            "2": "2d513de80a0b4df0"
+        },
+        "matchingOperator": "match-mapping",
+        "compDecompFct": "mapping-sent(4)"
+    },
+    "IP_iidLA": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "LAiid-DID"
+    },
+    "UDP_PortES": {
+        "targetValue": "1f90",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "UDP_PortLA": {
+        "targetValue": "2382",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "UDP_length": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "compute-UDP-length"
+    },
+    "UDP_checksum": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "compute-UDP-checksum"
+    },
+    "CoAP_version": {
+        "targetValue": "1",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "CoAP_type": {
+        "targetValue": "1",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "CoAP_tokenLength": {
+        "targetValue": "1",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "CoAP_code": {
+        "targetValue": "02",
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "CoAP_messageID": {
+        "targetValue": "000",
+        "matchingOperator": "MSB(12)",
+        "compDecompFct": "LSB(4)"
+    },
+    "CoAP_token": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "value-sent"
+    },
+    "CoAP_Uri-Path 1": {
+        "targetValue": "666f6f", // "foo"
+        "matchingOperator": "equal",
+        "compDecompFct": "not-sent"
+    },
+    "CoAP_Uri-Path 2": {
+        "targetValue": "",
+        "matchingOperator": "ignore",
+        "compDecompFct": "value-sent"
     }
 };
 
@@ -124,12 +236,13 @@ CD.initializeCD();
 
 // Adding a rule to the CDF context (for the moment must be added in order)
 CD.addCompressionRule(rule0);
+CD.addCompressionRule(rule1);
 
 // Load IIDs obtained from L2 (ESiid, LAiid)
 CD.loadIIDs("70b3d549925aa619","ada4dae3ac12676b"); // Examples of IIDs
 
 // Parsing the compressed packet received
-CD.parseCompressedPacket("001210a83190089003903c0"); // Example of a compressed message with rule0
+CD.parseCompressedPacket("011210a36261728302002f"); // Example of a compressed message with rule0
 
 // Decompression of the header according to the rule
 CD.decompressHeader();
